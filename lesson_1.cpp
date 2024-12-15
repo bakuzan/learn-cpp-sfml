e2bbaadc687d5f7a2f08bd49e374425753dfcb3c
bakuzan <imannoyed4days@hotmail.co.uk>
2024-12-15 19:02:27 +0000

    Changed setup file name; Lesson one code

---
+++ lesson_1.cpp
@@ -0,0 +1,20 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(512, 512), "SFML Tutorial", sf::Style::Close | sf::Style::Resize);
+
+    while (window.isOpen())
+    {
+        sf::Event evnt;
+        while (window.pollEvent(evnt))
+        {
+            if (evnt.type == evnt.Closed)
+            {
+                window.close();
+            }
+        }
+    }
+
+    return 0;
+}
